df5e52110a7d7f3da36ba238ab8629bf6ab6623e
Adam Martin <adam.madram@gmail.com>
2019-02-24 13:41:04 -0600

    Initial gdx-setup run

---
+++ desktop/src/com/greenstargames/pq/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.greenstargames.pq.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.greenstargames.pq.ProjectQGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectQGame(), config);
+	}
+}
